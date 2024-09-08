131d73da949982a4c45b1d833c611e19fb8e1133
asdfasdfad <m2mcvica@uwaterloo.ca>
2024-09-08 18:08:10 -0400

    Thinking character animation

--- index.js
+++ index.js
@@ -309,6 +309,7 @@ function drawGorilla(player) {
   drawGorillaLeftArm(player);
   drawGorillaRightArm(player);
   drawGorillaFace(player);
+  drawGorillaThoughtBubbles(player);
 
   ctx.restore();
 }
@@ -420,6 +421,33 @@ function drawGorillaFace(player) {
     ctx.quadraticCurveTo(0, 60, 5, 56);
   }
   ctx.stroke();
+}
+
+function drawGorillaThoughtBubbles(player) {
+  if (state.phase === 'aiming') {
+    const currentPlayerIsComputer =
+      (numberOfPlayers === 0 && state.currentPlayer === 1 && player === 1) ||
+      (numberOfPlayers !== 2 && state.currentPlayer === 2 && player === 2);
+
+    if (currentPlayerIsComputer) {
+      ctx.save();
+      ctx.scale(1, -1);
+
+      ctx.font = '20px sans-serif';
+      ctx.textAlign = 'center';
+      ctx.fillText('?', 0, -90);
+
+      ctx.font = '10px sans-serif';
+
+      ctx.rotate((5 / 180) * Math.PI);
+      ctx.fillText('?', 0, -90);
+
+      ctx.rotate((-10 / 180) * Math.PI);
+      ctx.fillText('?', 0, -90);
+
+      ctx.restore();
+    }
+  }
 }
 
 function drawBomb() {
